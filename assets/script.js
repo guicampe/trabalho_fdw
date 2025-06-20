21c7efdc793f99caad9314ab734d0f243446d720
Guilherme Ramos Campestrini <gramoscampestrini@gmail.com>
2025-06-20 20:31:59 -0300

    revisao de funcao e adicionado comentarios

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,20 @@
+// acesso ao icone (botao)
 const botoesExpandir = document.querySelectorAll(".material-symbols-outlined");
 
+// funcao para expandir o conteudo do projeto
 botoesExpandir.forEach(botao => {
     botao.addEventListener("click", () => {
+        // div que envolve o botao
         const containerExpandir = botao.parentElement;
+        // retorna proximo elemento irmao
         const projeto = containerExpandir.nextElementSibling;
+        // expande a div ou recolhe
         projeto.classList.toggle("ativo");
 
+        // efeito de giro no icone
         if (projeto.classList.contains("ativo")) {
             botao.classList.add("girando");
         } else {
-            botao.textContent = "stat_minus_1";
             botao.classList.remove("girando");
         }
     });
